f045eb9677a28b4ce5fcca4f5cda2675feb32551
venkatraviqrc <venkatraviqrc@gmail.com>
2026-04-18 10:06:35 +0000

    jenkins file updated

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,22 +2,22 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
- 
     public String home() {
         return "Home Page Working";
-   
+    }
 
     @GetMapping("/demo")
     public String demo() {
         return "Home Page Working v2";
     }
-@GetMapping("/hello")
-public String hello() {
-    return "Hello DevOps!";
-}
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello DevOps!";
+    }
 }
