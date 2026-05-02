304f08c2e98de1c79815b3e49d3d643f24c4a470
Ubuntu <ubuntu@ip-172-31-35-145.ap-south-1.compute.internal>
2026-05-02 10:07:00 +0000

    some changes

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "App Running";
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
